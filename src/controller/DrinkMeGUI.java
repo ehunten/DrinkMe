3fb584c3ffdbda459bd6b752dbc494e89aaa630b
Hannah Ingle <Hannah Ingle>
2016-11-21 14:37:46 -0700

    commit again after pulling master

--- src/controller/DrinkMeGUI.java
+++ src/controller/DrinkMeGUI.java
@@ -4,5 +4,8 @@ public class DrinkMeGUI {
 	
 	//this is what i plan on working on today a litte bit
 	//attemphting to push into the master
+	
+	//alright didnt read the also first
+	//going to try and fetch and recommit this
 
 }

f8a01637016857b96212d21123eb359ea7a3b20a
ehunten <e.hunten@gmail.com>
2016-12-01 13:44:41 -0700

    Revert "Add Drink with hangover potenetial and background test"

--- src/guiStuff/DrinkMeGUI.java
+++ src/guiStuff/DrinkMeGUI.java
@@ -11,12 +11,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.awt.event.ActionEvent;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import java.awt.Font;
 import java.awt.Color;
@@ -146,10 +141,9 @@ public class DrinkMeGUI extends JFrame {
 		lblNewLabel.setBounds(5, 28, 574, 32);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("");	
-		lblNewLabel_1.setIcon(new ImageIcon("src/mainBack.jpg"));
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(".\\.\\bin\\mainMenuBackground.jpg"));
 		lblNewLabel_1.setBounds(0, 11, 584, 429);
 		contentPane.add(lblNewLabel_1);
-
 	}
 }
